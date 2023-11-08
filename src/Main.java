b90a5ce4adec1ac9c9a0c0b90979d0f1f613cb51
Ulogii121 <ulogii121@gmail.com>
2023-11-08 23:03:35 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // Пишем свой код
+        // Когда все написали, и нужно запушить код в Гитхаб, делаем следующее:
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
