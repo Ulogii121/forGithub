15da4312b2924a4f9e88d4cbeead99382c69d191
Ulogii121 <ulogii121@gmail.com>
2023-11-08 23:06:11 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ public class Main {
 
         for (int i = 1; i <= 5; i++) {
 
+            // Теперь код в гитхабе!
+            // Если что-то поменяли, и нужно обновленный код залить в Гитхаб, делаем так:
+
+            // Проверяем, чтобы не было красных файлов (который я убрал сейчас),
+            // Иначе эти файлы не увидятся и не запушатся в Гитхаб
+
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
