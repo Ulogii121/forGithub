02ae7d62fb5bbc6aa13d23d447d7acc7b7debc27
Ulogii121 <ulogii121@gmail.com>
2023-11-26 22:25:18 +0300

    cats methods

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,118 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(feedCatsWithConsoleValidation()));
+    }
 
-        // Пишем свой код
-        // Когда все написали, и нужно запушить код в Гитхаб, делаем следующее:
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-
-            // Теперь код в гитхабе!
-            // Если что-то поменяли, и нужно обновленный код залить в Гитхаб, делаем так:
-
-            // Проверяем, чтобы не было красных файлов (который я убрал сейчас),
-            // Иначе эти файлы не увидятся и не запушатся в Гитхаб
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static boolean[] isCatsHungry(int[] cats) {
+        boolean[] eat = new boolean[cats.length];
+        for (int i = 0; i < eat.length; i++) {
+            if (cats[i] < 100) {
+                eat[i] = false;
+            } else eat[i] = true;
         }
+        return eat;
+    }
+
+    public static int[] feedCats(int[] cats) {
+        int[] eat = new int[cats.length];
+        for (int i = 0; i < eat.length; i++) {
+            if (cats[i] < 100) {
+                int feed = 100 - cats[i];
+                eat[i] = cats[i] + feed;
+            } else eat[i] = cats[i];
+        }
+        return eat;
+    }
+
+    public static int[] feedCatsWithValidations(int[] cats) {
+        int[] eat = new int[cats.length];
+        for (int i = 0; i < eat.length; i++) {
+            if (cats[i] < 100) {
+                int feed = 100 - cats[i];
+                eat[i] = cats[i] + feed;
+            } else {
+                eat[i] = 100;
+            }
+        }
+        return eat;
+    }
+
+    public static int[] feedCatsWithConsole() {
+        Scanner inputFromConsole = new Scanner(System.in);
+        System.out.println("Введите процент сытости 1го котика: ");
+        int firstPercent = inputFromConsole.nextInt();
+        System.out.println("Введите процент сытости 2го котика: ");
+        int secondPercent = inputFromConsole.nextInt();
+        System.out.println("Введите процент сытости 3го котика: ");
+        int thirdPercent = inputFromConsole.nextInt();
+        inputFromConsole.close();
+        int[] cats = {firstPercent, secondPercent, thirdPercent};
+        int[] eat = new int[cats.length];
+        for (int i = 0; i < eat.length; i++) {
+            if (cats[i] < 100) {
+                int feed = 100 - cats[i];
+                eat[i] = cats[i] + feed;
+            } else {
+                eat[i] = 100;
+            }
+        }
+        return eat;
+    }
+
+    public static int[] feedCatsWithDynamicArraySize() {
+        Scanner inputFromConsole = new Scanner(System.in);
+        System.out.println("Введите размер массива котиков (сколько их будет): ");
+        int catSize = inputFromConsole.nextInt();
+        int[] cats = new int[catSize];
+        for (int i = 0; i < catSize; i++) {
+            System.out.println("Введите процент сытости " + (i + 1) + "го котика: ");
+            int eatPercent = inputFromConsole.nextInt();
+            cats[i] = eatPercent;
+        }
+        inputFromConsole.close();
+        int[] eat = new int[cats.length];
+        for (int i = 0; i < eat.length; i++) {
+            if (cats[i] < 100) {
+                int feed = 100 - cats[i];
+                eat[i] = cats[i] + feed;
+            } else {
+                eat[i] = 100;
+            }
+        }
+        return eat;
+    }
+
+    public static int[] feedCatsWithConsoleValidation() {
+        Scanner inputFromConsole = new Scanner(System.in);
+        System.out.println("Введите размер массива котиков (сколько их будет): ");
+        int catSize = inputFromConsole.nextInt();
+        int[] cats = new int[catSize];
+        for (int i = 0; i < catSize; i++) {
+            boolean isValidPercent = true;
+            int eatPercent = 0;
+            while (isValidPercent) {
+                System.out.println("Введите процент сытости " + (i + 1) + "го котика: ");
+                eatPercent = inputFromConsole.nextInt();
+                if (inputFromConsole.nextInt() < 0 || inputFromConsole.nextInt() > 100) {
+                    System.out.println("Вы ввели неверное значение! Попробуйте ещё раз.");
+                }
+                isValidPercent = false;
+            }
+            cats[i] = eatPercent;
+        }
+        inputFromConsole.close();
+        int[] eat = new int[cats.length];
+        for (int i = 0; i < eat.length; i++) {
+            if (cats[i] < 100) {
+                int feed = 100 - cats[i];
+                eat[i] = cats[i] + feed;
+            } else {
+                eat[i] = 100;
+            }
+        }
+        return eat;
     }
 }
